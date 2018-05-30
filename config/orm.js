624abf107a3f989f5ecbc090b2f625b136b9ca71
Mapi Pina <mpina09@gmail.com>
2018-05-30 14:51:03 -0400

    changed from connection.connect to connection.getConnection

--- config/orm.js
+++ config/orm.js
@@ -1,5 +1,6 @@
 var connection = require("./connection.js");
-connection.connect(function(err) {
+
+connection.getConnection(function(err) {
   if (err) {
     console.error("error connecting: " + err.stack);
     return;
