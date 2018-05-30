c68b08bfa021b0de2ebdca4713c597de5f0cd3f8
Mapi Pina <mpina09@gmail.com>
2018-05-30 13:08:05 -0400

    testing new db config

--- config/connection.js
+++ config/connection.js
@@ -2,13 +2,27 @@
 const mysql = require("mysql");
 require('dotenv').config();
 
-var connection = mysql.createConnection({
-  port: 3306,
-  host: "localhost",
-  user: "root",
-  password: "Nole1990!",
-  database: "burgers_db"
+//establish a pool connection
+var pool = mysql.createPool({
+  connectionLimit: 10,
+  host: "us-cdbr-iron-east-05.cleardb.net",
+  user: "b3237c9b839aa8:af17f223",
+  password: "us-cdbr-iron-east-04.cleardb.net",
+  database: "heroku_e0fe6548ff59ee9"
 });
+
+
+
+
+module.exports = pool;
+
+// var connection = mysql.createConnection({
+//   port: 3306,
+//   host: "localhost",
+//   user: "root",
+//   password: "Nole1990!",
+//   database: "burgers_db"
+// });
 
 // Make connection.
 connection.connect(function(err) {
