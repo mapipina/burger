600cd146133286d6bf8adcd457ffaa75299e7aab
Mapi Pina <mpina09@gmail.com>
2018-05-30 14:29:16 -0400

    fixed issue with uppercase letter

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.use(bodyParser.json());
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
-const routes = require("./controllers/burgers_Controller.js");
+const routes = require("./controllers/burgers_controller.js");
 
 app.use(routes);
 
