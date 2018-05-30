f60ac8069541590a0a6e9d651eb91dffb7920fe0
Mapi Pina <mpina09@gmail.com>
2018-05-30 08:05:32 -0400

    added line to db schema.sql for cleardb

--- db/schema.sql
+++ db/schema.sql
@@ -1,6 +1,8 @@
 CREATE DATABASE burgers_db;
-USE burgers_db;
+-- USE burgers_db;
+USE heroku_e0fe6548ff59ee9;
 
+DROP TABLE IF EXISTS burgers;
 CREATE TABLE burgers
 (
 	id int NOT NULL AUTO_INCREMENT,
