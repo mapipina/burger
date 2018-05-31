a6e7dc05d3f08e63ce5348bb184e82362c6b4108
Mapi Pina <mpina09@gmail.com>
2018-05-31 19:31:39 -0400

    FINALLY got heroku deployment working. now inserting rest of ORM logic

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -10,17 +10,17 @@ router.get('/', function (req, res) {
 	
 });
 
-// router.post('/burgers/update', function(req, res){
-// 	burger.update(req.body.burger_id, function(result){
-// 		console.log(result);
-// 		res.redirect('/');
-// 	});
-// });
+router.post('/burgers/update', function(req, res){
+	burger.update(req.body.burger_id, function(result){
+		console.log(result);
+		res.redirect('/');
+	});
+});
 
-// router.post('/burgers/create', function(req, res){
-// 	burger.insert(req.body.burger_name, function(result){
-// 		res.redirect('/');
-// 	});
-// });
+router.post('/burgers/create', function(req, res){
+	burger.insert(req.body.burger_name, function(result){
+		res.redirect('/');
+	});
+});
 
 module.exports = router;
