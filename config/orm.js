fafb21d5218cdaee2573d858827f4170b60434f8
Mapi Pina <mpina09@gmail.com>
2018-05-30 21:17:09 -0400

    testing new db orm

--- config/orm.js
+++ config/orm.js
@@ -1,33 +1,37 @@
 var connection = require("./connection.js");
 
-connection.getConnection(function(err) {
-  if (err) {
-    console.error("error connecting: " + err.stack);
-    return;
-  }
-  console.log("connected as id " + connection.threadId);
-});
+var con = function(callback){
+    connectionPool.getConnection(function(err, connection) {
+         if (err){
+            connection.release();
+            throw (err);          
+        }
+        callback(connection); 
+    });
+};
 
 var orm = {
   selectAll: function(tableInput, cb) {
     var queryString = "SELECT * FROM " + tableInput + ";";
-    connection.query(queryString, function(err, result) {
-      if (err) throw err;
-      cb(result);
-    });
-  },
-  insertOne: function(tableInput, val, cb) {
-    connection.query('INSERT INTO '+tableInput+ " (burger_name) VALUES ('"+val+"');", function(err, result) {
-      if (err)
-        throw err;
-      cb(result);
-    });
-  },
-  updateOne: function(tableInput, condition, cb){
-    connection.query('UPDATE ' +tableInput+ ' SET devoured=true WHERE id='+condition+';', function (err, result) {
-      if (err) throw err
-        cb(result);
+    con(connection=>{connection.query(queryString, function(err, result) {
+              if (err) throw err;
+              connection.release();
+              cb(result);
+            });
       })
-    }
+  },
+  // insertOne: function(tableInput, val, cb) {
+  //   connection.query('INSERT INTO '+tableInput+ " (burger_name) VALUES ('"+val+"');", function(err, result) {
+  //     if (err)
+  //       throw err;
+  //     cb(result);
+  //   });
+  // },
+  // updateOne: function(tableInput, condition, cb){
+  //   connection.query('UPDATE ' +tableInput+ ' SET devoured=true WHERE id='+condition+';', function (err, result) {
+  //     if (err) throw err
+  //       cb(result);
+  //     })
+  //   }
   };
 module.exports = orm;
