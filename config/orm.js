a1fb9be24ff0bcc0074fce6706ae7f464d8668fa
Mapi Pina <mpina09@gmail.com>
2018-05-31 20:51:32 -0400

    testing out connection pool in order to stop server from timing out and restarting heroku dynos

--- config/orm.js
+++ config/orm.js
@@ -1,25 +1,28 @@
-var connection = require("./connection.js");
+var pool = require("./connection.js");
 
 
 var orm = {
   selectAll: function(tableInput, cb) {
     var queryString = "SELECT * FROM " + tableInput + ";";
-    connection.query(queryString, function(err, result) {
+    pool.query(queryString, function(err, result) {
               if (err) throw err;
               cb(result);
+              connection.release();
             });
   },
   insertOne: function(tableInput, val, cb) {
-    connection.query('INSERT INTO '+tableInput+ " (burger_name) VALUES ('"+val+"');", function(err, result) {
+    pool.query('INSERT INTO '+tableInput+ " (burger_name) VALUES ('"+val+"');", function(err, result) {
       if (err)
         throw err;
       cb(result);
+      connection.release();
     });
   },
   updateOne: function(tableInput, condition, cb){
-    connection.query('UPDATE ' +tableInput+ ' SET devoured=true WHERE id='+condition+';', function (err, result) {
+    pool.query('UPDATE ' +tableInput+ ' SET devoured=true WHERE id='+condition+';', function (err, result) {
       if (err) throw err
         cb(result);
+        connection.release();
       })
     }
   };
