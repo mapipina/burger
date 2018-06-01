4c7924b97e1b691d84de74f4e883d47956c36696
Mapi Pina <mpina09@gmail.com>
2018-05-31 20:55:28 -0400

    removed connectionpool, not agreeing with heroku

--- config/connection.js
+++ config/connection.js
@@ -2,38 +2,21 @@
 var mysql = require("mysql");
 require('dotenv').config();
 
-var pool = mysql.createPool({
+//establish a pool connection
+var connection = mysql.createConnection({
   host: "us-cdbr-iron-east-04.cleardb.net",
   user: process.env.DB_USER,
   password: process.env.DB_PASS,
   database: "heroku_e0fe6548ff59ee9"
 });
 
-pool.getConnection(function(err, connection) {
+module.exports = connection;
+
+// Make connection.
+connection.connect(function(err) {
   if (err) {
     console.error("error connecting: " + err.stack);
     return;
   }
   console.log("connected as id " + connection.threadId);
 });
-
-module.exports = pool;
-
-// //establish a pool connection
-// var connection = mysql.createConnection({
-//   host: "us-cdbr-iron-east-04.cleardb.net",
-//   user: process.env.DB_USER,
-//   password: process.env.DB_PASS,
-//   database: "heroku_e0fe6548ff59ee9"
-// });
-
-// module.exports = connection;
-
-// // Make connection.
-// connection.connect(function(err) {
-//   if (err) {
-//     console.error("error connecting: " + err.stack);
-//     return;
-//   }
-//   console.log("connected as id " + connection.threadId);
-// });
--- config/orm.js
+++ config/orm.js
@@ -1,28 +1,25 @@
-var pool = require("./connection.js");
+var connection = require("./connection.js");
 
 
 var orm = {
   selectAll: function(tableInput, cb) {
     var queryString = "SELECT * FROM " + tableInput + ";";
-    pool.query(queryString, function(err, result) {
+    connection.query(queryString, function(err, result) {
               if (err) throw err;
               cb(result);
-              connection.release();
             });
   },
   insertOne: function(tableInput, val, cb) {
-    pool.query('INSERT INTO '+tableInput+ " (burger_name) VALUES ('"+val+"');", function(err, result) {
+    connection.query('INSERT INTO '+tableInput+ " (burger_name) VALUES ('"+val+"');", function(err, result) {
       if (err)
         throw err;
       cb(result);
-      connection.release();
     });
   },
   updateOne: function(tableInput, condition, cb){
-    pool.query('UPDATE ' +tableInput+ ' SET devoured=true WHERE id='+condition+';', function (err, result) {
+    connection.query('UPDATE ' +tableInput+ ' SET devoured=true WHERE id='+condition+';', function (err, result) {
       if (err) throw err
         cb(result);
-        connection.release();
       })
     }
   };
